ecc92fe7d07eb94ae8c3c439a2800482d3f11346
ArulKevin2004 <131542482+ArulKevin2004@users.noreply.github.com>
2024-12-07 21:04:52 +0530

    File Page Added

--- polly-scribe/src/App.jsx
+++ polly-scribe/src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import HomePage from './components/Homepage'
+import FileDisplay from './components/FileDisplay'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvailable = file || audioStream
+
+  function handleAudioReset(){
+    setFile(null)
+    setAudioStream(null)
+  }
 
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
-        <HomePage />
-      </section>
+        {isAudioAvailable ? (
+          <FileDisplay handleAudioReset={handleAudioReset} file = {file} audioStream = {setAudioStream}/>
+        ) : (
+        <HomePage setFile = {setFile} setAudioStream = {setAudioStream}/>
+        )}
+      </section> 
       <h1 className='text-5xl'>Hello</h1>
       <footer>
 
---
+++ polly-scribe/src/components/FileDisplay.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FileDisplay = (props) => {
+    const {handleAudioReset, file, audioStream} = props
+  return (
+    <main className='flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 md:gap-5 text-center justify-center pb-20 w-fit max-w-full mx-auto'>
+      <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>Your 
+        <span className='text-blue-400 bold'>File</span></h1>
+        <div className='flex flex-col text-left mx-auto mb-7'>
+            <h2 className='font-semibold text-2xl my-1'>Name</h2>
+            <p className='text-slate-400 text-lg'>{file.name}</p>
+        </div>
+        <div className='flex justify-between items-center gap-96'>
+            <button onClick={handleAudioReset} className='text-2xl text-slate-400 hover:text-blue-400'>Reset</button>
+            <button className='flex gap-2 items-center text-xl text-slate-500 specialBtn rounded-xl px-4 py-2 hover:text-blue-400'>
+                <p>Transcribe</p>
+                <i class="fa-solid fa-pencil"></i>
+            </button>
+        </div>
+    </main>
+  )
+}
+export default FileDisplay
